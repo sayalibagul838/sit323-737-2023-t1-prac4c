869f7f557cb9fc377c262f7c0bb01cfb902f94c0
sayali <sayalibagul06@gmail.com>
2023-03-29 20:15:54 +1100

    first commit

---
+++ node.js
@@ -0,0 +1,209 @@
+const express= require("express");
+const res = require("express/lib/response");
+const app= express();
+const fs = require('fs');
+const winston = require('winston');
+const _ = require('lodash');
+
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const passportJWT = require("passport-jwt");
+const JWTStrategy   = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const jwt = require('jsonwebtoken');
+
+const payload ={
+    id: 1,
+    name: 'sayali',
+    password: '12345'
+};
+const secret = 'your_jwt_secret';
+
+const token = jwt.sign(payload, secret);
+console.log(token);
+const users = [
+    {
+        id: 1,
+        name: 'sayali',
+        password: '12345'
+    },
+    {
+        id: 2,
+        name: 'gargi',
+        password: '09876'
+    }
+];
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey   : 'your_jwt_secret'
+};
+
+const strategy = new JWTStrategy(jwtOptions, (jwt_payload, next) => {
+    console.log('payload received', jwt_payload);
+    // usually this would be a database call:
+    const user = users[_.findIndex(users, {id: jwt_payload.id})];
+    if (user) {
+        next(null, user);
+    } else {
+        next(null, false);
+    }
+});
+
+passport.use(strategy);
+ 
+app.use(passport.initialize());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'calculate-service' },
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //4
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  });
+  
+  //
+  // If we're not in production then log to the `console` with the format:
+  // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+  //
+  if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+      format: winston.format.simple(),
+    }));
+  }
+const add= (n1,n2) => {
+    return n1+n2;
+}
+const sub= (n1,n2) => {
+    return n1-n2;
+}
+
+const mul= (n1,n2) => {
+    return n1*n2;
+}
+
+const div= (n1,n2) => {
+    return n1/n2;
+}
+
+
+app.get("/add", passport.authenticate('jwt', { session: false }) , (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for addition');
+    const result = add(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/sub", passport.authenticate('jwt', { session: false }) , (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for addition');
+    const result = sub(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/mul", passport.authenticate('jwt', { session: false }) , (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for addition');
+    const result = mul(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+app.get("/div", passport.authenticate('jwt', { session: false }) , (req,res)=>{
+    try{
+    const n1= parseFloat(req.query.n1);
+    const n2=parseFloat(req.query.n2);
+    if(isNaN(n1)) {
+        logger.error("n1 is incorrectly defined");
+        throw new Error("n1 incorrectly defined");
+    }
+    if(isNaN(n2)) {
+        logger.error("n2 is incorrectly defined");
+        throw new Error("n2 incorrectly defined");
+    }
+    
+    if (n1 === NaN || n2 === NaN) {
+        console.log()
+        throw new Error("Parsing Error");
+    }
+    logger.info('Parameters '+n1+' and '+n2+' received for addition');
+    const result = div(n1,n2);
+    res.status(200).json({statuscocde:200, data: result }); 
+    } catch(error) { 
+        console.error(error)
+        res.status(500).json({statuscocde:500, msg: error.toString() })
+      }
+});
+
+
+const port=3040;
+app.listen(port,()=> {
+    console.log("hello i'm listening to port " +port);
+})
